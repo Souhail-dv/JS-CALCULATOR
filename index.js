2746662ef97a1e402d7ac10ad01ed2a6be52152c
souhailad55 <souhailaaddidev@gmail.com>
2025-06-02 13:32:54 +0100

    Add function to clear display

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const display = document.getElementById("display");
 function appendToDisplay(input){
      display.value = input + display.value;
 }
+function  clearDisplay(){
+    display.value = "";
+}
