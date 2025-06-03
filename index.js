1d0cd60c3b108d7416537466bdd37e9d7870b4c3
souhailad55 <souhailaaddidev@gmail.com>
2025-06-04 00:04:30 +0100

    Add calculate function to evaluate expression with error handling

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function appendToDisplay(input){
 function  clearDisplay(){
     display.value = "";
 }
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
